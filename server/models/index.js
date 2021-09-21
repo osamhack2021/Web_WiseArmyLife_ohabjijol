822f9a3cfe0a1f6b5895c2cc39abb6266fbea0aa
SeungbeomHu <77676613+SeungbeomHu@users.noreply.github.com>
2021-09-21 08:22:19 +0000

    병자격인증 사격 테이블 구성

--- server/models/index.js
+++ server/models/index.js
@@ -4,6 +4,10 @@
 const Sequelize = require('sequelize');
 const User = require('./users');
 const Post = require('./post');
+
+const Shooting = require('./applies/shooting');
+
+
 const Comment = require('./comment');
 
 const env = process.env.NODE_ENV || 'development';
@@ -12,17 +16,24 @@ const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password,config);
 
+
 db.sequelize = sequelize;
 db.User = User;
 db.Post = Post;
+db.Shooting = Shooting;
+
 db.Comment = Comment;
+
 
 User.init(sequelize);
 Post.init(sequelize);
 Comment.init(sequelize);
+Shooting.init(sequelize);
 
 User.associate(db);
 Post.associate(db);
 Comment.associate(db);
+
+
 
 module.exports = db;
