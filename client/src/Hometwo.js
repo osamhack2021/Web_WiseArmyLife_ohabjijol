5ab400cc5ada6914cc04049363c148681f2e2d32
codrae <88239626+codrae@users.noreply.github.com>
2021-09-28 14:49:08 +0000

    good

--- client/src/Hometwo.js
+++ client/src/Hometwo.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import "./Hometwo.css";
+import "./Home.css";
 
 
 function Home() {
@@ -11,6 +11,10 @@ function Home() {
    paddingBottom: "20px"
   }
 
+  const smallStyle = {
+    fontSize: "12px",
+    textAlign:"center"
+  }
 
   return (
 
@@ -38,18 +42,51 @@ function Home() {
 
     {/* 여기까지가 상위 배너   // 링크는 나중에 추가! 일단 틀만.. */}
 
+<div className="topBanner">
     <img src={ require('../public/img/wal.jpg') } alt ="상위 배너 사진"/>
     <button>circle</button>
-    
-    {/* 버튼으로 옆으로 넘어가게 하는거 설정할꺼 모양은 나중에
-     일정 시간, 혹은 버튼 클릭시 새로운 사진으로 넘어가기
-     그리고 각각의 사진에 다른 링크 걸어놓는식!! */}
+    </div>
+
+<div className="content">
+
+  <h2>병기본평가+</h2>
+  <div className="basicTest">
+    <p>사격<br></br>
+    <div style = {smallStyle}>
+    사격 평가 일정 안내 및 신청
+    </div>
+    </p>
+
+    <p>정신전력평가</p>
+
+    <p>체력</p>
+
+    <p>구급법 및 화생방</p>
+
+    <p>각개전투</p>
+    <p>주특기</p>
+    </div>
+
+    <h2>공지사항+</h2>
+    <div className="notice">
+   <p>군장병 코로나 방역수칙 안내</p>
+  <p>자유시간 관련 공지</p>
+  <p>휴가 및 외박 일정 안내</p>
+  </div>
+
+  <h2>커뮤니티+</h2>
+<div className="community">
+   <p>오늘의 급식</p>
+  <p>마음의 편지</p>
+  </div>
+
+
+  </div>
 
 
 
 
-
-  <div className ="footerContainer">
+<div className ="footerContainer">
   <p>슬기로운 병영생활</p>
   <div className="footerTwo">
   <p>대표이메일 seulgiarmy@naver.com</p>
