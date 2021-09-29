5bd36a26e2bd2568ac5466590ff41acce643edc5
hereokay <hereokay88@gmail.com>
2021-09-29 20:26:05 +0900

    bulid : error 제거

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState,Component} from "react";
 import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import Header from './HeadList/Header';
-import Footer from './HeadList/Footer';
+import Header from './components/HeadList/Header';
+import Footer from './components/HeadList/Footer';
 
 
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import App from '../App';
+import App from './App';
+
 
 
 ReactDOM.render((
