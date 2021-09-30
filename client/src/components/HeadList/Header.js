ed59c0fe5119519ed902ed4f87814df9b2325198
hereokay <hereokay88@gmail.com>
2021-09-30 22:34:13 +0900

    ADD : 0928 용진

--- client/src/components/HeadList/Header.js
+++ client/src/components/HeadList/Header.js
@@ -10,7 +10,6 @@ import AuthRoute from "../../Custom/AuthRoute";
 import { signIn } from './../../Custom/Auth';
 import LogoutButton from "./LogoutButton";
 import LoginForm from "./LoginForm";
-import Button from './Button';
 import PublicRoute from "../../Custom/PublicRoute";
 import { setCookie,getCookie } from "../../Custom/cook";
 import { isLogin } from "../../Custom/isLogin";
@@ -20,6 +19,7 @@ const Header = () => {
     const [user,setUser] = useState(null);
     let authenticated = user !=null;
 
+    
 
     const login = ({ user_id, user_pw }) => {
         setCookie('login',true)
@@ -29,73 +29,63 @@ const Header = () => {
         setCookie('login',false)
         return setUser(null);
     }
+
+    const headrStyle = {
+        fontSize: "32px",
+        textAlign: "center",
+        paddingBottom: "20px"
+      };
+    
+    
     
     return (
-        <div className="header">
-            <h1 className="topHeader">슬기로운 병영생활</h1>
-            
-            <div>
-                <ul className="topList">
-                    <Router>
-                        <li className="topListItem">
-                            <Link to="/">
-                                <div>홈</div>
-                            </Link>
-                        </li>
-                        <li className="topListItem">
-                            <Link to="/assess">
-                                <div>병기본평가</div>
-                            </Link>
-                        </li>
-                        <li className="topListItem">
-                            <Link to="/community">
-                                <div>커뮤니티</div>
-                            </Link>
-                        </li>
-                        <li className="topListItem">
-                            <Link to="/my">
-                                <div>마이페이지</div>
-                            </Link>
-                        </li>
-                        <li className="topListItem">
-                            {authenticated ? (
-                                <LogoutButton auth={authenticated} logout={logout} />
-                                ) : (
-                                <Link to="/login">
-                                    <button>Login</button>
-                                </Link>
-                            )}
-                        </li>
-                        
-                        <li className="topListItem">
-                            <Link to="/auth">
-                                <div>회원가입</div>
-                            </Link>
-                        </li>
-                        <li>
-                        <Button />
-                        </li>
-                        <Switch>
-                            <PublicRoute exact path="/" restricted={false} auth={authenticated} component={DetailHome} />
-                            <Route path="/login" render={(props) => (
-                                <LoginForm
-                                    authenticated={authenticated}
-                                    login={login}
-                                    {...props}
-                                />
-                                )}
-                            />
-                            <PublicRoute path="/community" restricted={false} auth={authenticated}  component={Community} />
-                            <PublicRoute path="/assess" restricted={false} auth={authenticated} component={Assess} />
-                            <AuthRoute path="/my" authenticated={authenticated} render={ () => <My />} />
-                        </Switch>
+        <React.Fragment>
 
-                    </Router>
-
-                </ul>
+            <div className="Header" style={headrStyle}>
+                <h1>슬기로운 병영생활</h1>
             </div>
-            
-        </div>
+            <Router>
+                <div className="container">
+                    <Link to="/">
+                        <div>홈</div>
+                    </Link>
+                    <Link to="/assess">
+                        <div>병기본평가</div>
+                    </Link>
+                    <Link to="/community">
+                        <div>커뮤니티</div>
+                    </Link>
+                    <Link to="/my">
+                        <div>마이페이지</div>
+                    </Link>
+                    {authenticated ? 
+                    (
+                        <LogoutButton auth={authenticated} logout={logout} />
+                    ) : 
+                    (<Link to="/login">
+                        <button>Login</button>
+                    </Link>)}
+                    <Link to="/auth">
+                        <div>회원가입</div>
+                    </Link>
+                </div>
+                
+                <Switch>
+                    <PublicRoute exact path="/" restricted={false} auth={authenticated} component={DetailHome} />
+                    <Route path="/login" render={(props) => (
+                        <LoginForm
+                            authenticated={authenticated}
+                            login={login}
+                            {...props}
+                        />
+                        )}
+                    />
+                    <PublicRoute path="/community" restricted={false} auth={authenticated}  component={Community} />
+                    <PublicRoute path="/assess" restricted={false} auth={authenticated} component={Assess} />
+                    <AuthRoute path="/my" authenticated={authenticated} render={ () => <My />} />
+                </Switch>
+            </Router>
+        </React.Fragment>
     );
 };
 
