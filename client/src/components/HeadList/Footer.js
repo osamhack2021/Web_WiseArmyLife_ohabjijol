abe6a11039593f87970911afa28b6d40c6b08aa3
hereokay <hereokay88@gmail.com>
2021-10-02 21:56:22 +0900

    Del : clean code

--- client/src/components/HeadList/Footer.js
+++ client/src/components/HeadList/Footer.js
@@ -5,9 +5,7 @@ const Footer = () => {
     return (
         <div className="footerContainer">
             <p>슬기로운 병영생활</p>
-            <div className="footerTwo">
                 <p>대표이메일 seulgiarmy@naver.com</p>
-            </div>
             <p>이용약관</p>
             <p>개인정보 처리방침</p>
       </div>
