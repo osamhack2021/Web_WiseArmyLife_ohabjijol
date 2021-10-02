320d48120a394e3f7fa3d9ac80534d970aa73db6
hereokay <hereokay88@gmail.com>
2021-10-02 22:40:26 +0900

    Edit : 파일구조 정리

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
-import Header from './components/HeadList/Header';
-import Footer from './components/HeadList/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 
 
