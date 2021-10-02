84bb5bb2a703fe970a533ff7b118891b7bc6a770
hereokay <hereokay88@gmail.com>
2021-10-02 22:59:53 +0900

    ADD : 게시판 라우터 추가

--- client/src/components/Community/Community.js
+++ client/src/components/Community/Community.js
@@ -1,19 +1,44 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import axios from 'axios';
 
 
 // match.params 사용
-
+// 
 const Community = ({match}) => {
+    const [data,setData] = useState(null);
+    
     useEffect(() => {
-        axios.get('/Community')
-        .then(res => console.log(res))
-        .catch()
+        const {forumId,pageIndex,postId} =match.params;
+        if(postId !==undefined){ // 특정게시판 특정 글
+            axios.get(`/Community/${forumId}/v/${postId}`)
+            .then(res => {
+                console.log(res)
+                setData(res);
+            })
+        } else if(pageIndex !==undefined){ // 특정게시판 특정페이지
+            axios.get(`/Community/${forumId}/${pageIndex}`)
+            .then(res => {
+                console.log(res)
+                setData(res);
+            })
+        } else if( forumId !==undefined){ // 특정게시판 첫페이지
+            axios.get(`/Community/${forumId}/1`)
+            .then(res => {
+                console.log(res)
+                setData(res);
+            })
+        } else{ // 그냥 커뮤니티
+            axios.get(`/Community`)
+            .then(res => {
+                console.log(res)
+                setData(res);
+            })
+        }
     },[])
     
     return (
         <div>
-            커뮤니티
+            커뮤니티 data : {data}
         </div>
     );
 };
