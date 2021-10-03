f5953255dab57983befbe7b6404fc6f7542c4364
hereokay <hereokay88@gmail.com>
2021-10-03 23:26:42 +0900

    edit : 헤더 css 회원가입 css 변경

--- client/src/components/Login/Auth.js
+++ client/src/components/Login/Auth.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import axios from 'axios';
-
+import './Auth.css'
 
 
 const submitClick = async data => {
@@ -49,15 +49,48 @@ const Auth = () => {
 
     
     return (
-        <form>
-            <input placeholder="군번" name="user_id" value={user_id} onChange={onChange}/>
-            <input placeholder="이름" name="user_name" value={user_name} onChange={onChange}/>
-            <input placeholder="비밀번호" name="user_pw" value={user_pw} onChange={onChange}/>
-            <input placeholder="직책" name="position" value={position} onChange={onChange}/>
-            <input placeholder="간부" name="isExecutive" value={isExecutive} onChange={onChange}/>
+        <div>
+            <span className="textSignup">회원가입 +</span>
+            <div>
+                <input className="exeInput1" type="radio" name="selectEx"></input>
+                <span className="exeText">간부</span>
+                <input className="exeInput2" type="radio" name="selectEx"></input>
+                <span className="exeText">병사</span>
+            </div>
+            <div  className="signupForm">
+                <form>
+                    <div className="marginBox71" />
+                    <span className="unit">1. 군번</span>
+                    <div className="marginBox48" />
+                    <input className="idInput" placeholder="군번" name="user_id" value={user_id} onChange={onChange}/>
+                    <div className="marginBox48" />
 
-            <button onClick={onClick}>회원가입</button>
-        </form>
+                    <span className="unit">2. 이름</span>
+                    <div className="marginBox48" />
+                    <input  className="idInput" placeholder="이름" name="user_name" value={user_name} onChange={onChange}/>
+                    <div className="marginBox71" />
+
+                    <span className="unit">3. 비밀번호</span>
+                    <div className="marginBox48" />
+                    <input  className="idInput2" placeholder="비밀번호" name="user_pw" value={user_pw} onChange={onChange}/>
+                    <input  className="idInput" placeholder="비밀번호 확인" name="user_pw" value={user_pw} onChange={onChange}/>
+                    <div className="marginBox71" />
+
+                    <span className="unit">4. 직책</span>
+                    <div className="marginBox48" />
+                    <input className="idInput" placeholder="직책" name="position" value={position} onChange={onChange}/>
+                    <div className="marginBox71" />
+                    
+
+
+                    <div className="marginBox48" />
+                    <input placeholder="간부" name="isExecutive" value={isExecutive} onChange={onChange}/>
+                    <div className="marginBox48" />
+
+                    <span className="totalSignup" onClick={onClick}>회원가입</span>
+                </form>
+            </div>
+        </div>
     );
 };
 
