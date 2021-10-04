f93a8fae8a0f884ef93c8c0e62f1cc130d42d854
hereokay <hereokay88@gmail.com>
2021-10-04 18:29:59 +0900

    post 요청 성공

--- client/src/components/Assess/Assess.js
+++ client/src/components/Assess/Assess.js
@@ -10,19 +10,25 @@ const Assess = () => {
         })
     }
 
-    async function callPost() {
-        axios.post(`/assessment`,{
-            
-        })
-        .then(res => {
-            console.log(res.data)
+    const data= {
+        'hello':'world'
+    }
+
+    const callPost = () => {
+        console.log('a')
+        axios.post('/',data)
+        .then( response => {
+            console.log(data);
+
         })
     }
 
     return (
         <div>
             <button onClick={callGet}>get호출</button>
-            <button>post호출</button>
+            <div>
+                <button onClick={callPost}>post호출</button>
+            </div>
         </div>
     );
 };
