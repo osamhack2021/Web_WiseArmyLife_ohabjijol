ee6783f8e0ade8b8e9788f912d75cabe9d924fc8
hereokay <hereokay88@gmail.com>
2021-10-06 18:53:45 +0900

    태현형꺼 합치ㅣㄱ

--- server/models/index.js
+++ server/models/index.js
@@ -5,6 +5,8 @@ const { Sequelize, DataTypes } = require('sequelize');
 const User = require('./users');
 const Post = require('./community/post');
 const Shooting = require('./assessment/shooting');
+const MentalForce = require('./assessment/mentalForce');
+
 const Forum = require('./community/forum');
 const Comment = require('./community/comment');
 
@@ -21,23 +23,32 @@ db.Post = Post;
 db.Shooting = Shooting;
 db.Forum = Forum;
 db.Comment = Comment;
-
+db.MentalForce = MentalForce;
 
 const ShootingEvent = sequelize.define('ShootingEvent', {score: {type : DataTypes.INTEGER,defaultValue : -1},}, { timestamps: false });
+const MentalForceEvent = sequelize.define('MentalForceEvent', {score: {type : DataTypes.INTEGER,defaultValue : -1},}, { timestamps: false });
+const ExamEvent = sequelize.define('ExamEvent', {}, { timestamps: false });
+
+
+
 db.ShootingEvent = ShootingEvent;
+db.MentalForceEvent = MentalForceEvent;
+db.ExamEvent = ExamEvent;
+
 
 User.init(sequelize);
 Post.init(sequelize);
 Comment.init(sequelize);
 Shooting.init(sequelize);
 Forum.init(sequelize);
-
+MentalForce.init(sequelize);
 
 User.associate(db);
 Post.associate(db);
 Comment.associate(db);
 Shooting.associate(db);
 Forum.associate(db);
+MentalForce.associate(db);
 
 
 
--- server/routes/management/mentalForceController/createAssessment.js
+++ server/routes/management/mentalForceController/createAssessment.js
@@ -1,4 +1,5 @@
-const {Shooting ,ShootingEvent} = require('../../../models');
+const axios =require('axios');
+const {MentalForce ,MentalForceEvent} = require('../../../models');
 
 //사격의 C
 
@@ -29,15 +30,25 @@ createAssessment = async (req,res)=>{
         }
 
         
+        const exam = await axios.get('').catch(err=>{
+
+            const senderror = {
+                success: false,
+                data: "cannot Get Questions",
+            }
+            return res.json(senderror);
+
+        });//여기서 태현이형이 만든 시험 가져올 것 {id : int} 문제 아이디만 줘도 될듯. 만든 사격 아이디 + 가져온 시험정보id bulkCreate통해 사격평가와 문제를 N:M관계로 만듬
+
        
-        const createShootingAssessment = await Shooting.findOrCreate({
+        const createMentalForceAssessment = await MentalForce.findOrCreate({
             where : {
                 date : req.body.date,
                 
             },
             defaults : {
                 date : req.body.date,
-                time : req.bdoy.time,
+                time : req.body.time,
                 applicant_capacity : req.body.applicant_capacity,
                 number_of_applicant : 0,
                 expired:"Applying"
@@ -45,9 +56,11 @@ createAssessment = async (req,res)=>{
 
         });
 
-        console.log(createShootingAssessment[1]);
 
-        const isExist = !createShootingAssessment[1];
+
+        console.log(createMentalForceAssessment[1]);
+
+        const isExist = !createMentalForceAssessment[1];
         
         if(isExist){
 
