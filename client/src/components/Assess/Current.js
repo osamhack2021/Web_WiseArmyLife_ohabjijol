23fc526c6f2741ebef9afd78557f07e3c01cf69e
codrae <88239626+codrae@users.noreply.github.com>
2021-10-07 13:55:44 +0000

    만드는즁

--- client/src/components/Assess/Current.js
+++ client/src/components/Assess/Current.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Current.css';
 
 const Current = () => {
     return (
         <div>
-            용사2
+           <h2 className="peopleCheck">  인원확인 +  </h2>
+            <table className="currentConfirm">
+                <thead>
+                    <tr>
+                        <th>군번</th>  
+                        <th>이름</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>20-12345678</td>
+                        <td>홍길동</td>
+                    </tr>
+                </tbody>            
+                </table>
         </div>
     );
 };
