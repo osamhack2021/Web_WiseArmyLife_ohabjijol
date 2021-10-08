cac10601976e99e23932e52eeb912223e5c0ab3e
codrae <88239626+codrae@users.noreply.github.com>
2021-10-08 14:50:28 +0000

    commit

--- client/src/components/Community/mindLetter.js
+++ client/src/components/Community/mindLetter.js
@@ -8,9 +8,27 @@ const Letter = () => {
     }
 
     return (
-        <div className="title">
-            포스트화면
+        <>
+        <h1>마음의 편지+</h1>
+        <button>중대</button>
+        <button>대대</button> <br></br>
+        <button>진행중인 글</button>       
+        <button>완료된 글</button>
+
+        <div className="content">
+            <table>
+                <thead>
+                    <th>    제목     </th>
+                    <th>    날짜     </th>
+                    <th>    진행상태     </th>
+                </thead>
+                <tbody>
+                    <td></td>
+                </tbody>
+            </table>
         </div>
+
+        </>
     );
 };
 
