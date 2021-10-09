6e7ba79c7121cfdada0efbeaceb3d736df2d1f3b
codrae <88239626+codrae@users.noreply.github.com>
2021-10-09 10:31:43 +0000

    community?

--- client/src/components/Community/mindLetter.js
+++ client/src/components/Community/mindLetter.js
@@ -1,33 +1,99 @@
 import React from 'react';
-import "./mindLetter.css"
+import "./styles.css"
 
 const Letter = () => {
 
     const hTwoStyle = {
+        fontSize:"24px",
+        textAlign:"center",
+      
+    }
+
+    const buttonOne = {
+      border:"3px solid yellow",
+      padding:"10px",
+      margin:"10px",
+      textAlign:"center",
+      float:"center",
+    }
+
+    const tdStyle = {
+      borderBottom:"1px solid gray",
+      width:"300px",
+      height:"32px",
+    }
+
+    const buttonTwo = {
+      marginRight:"10px"
 
     }
 
+    const thStyle = {
+      hegight : "40px",
+      paddingBottom: "10px",
+      borderBottom:"1px solid white",
+    }
+ 
     return (
         <>
-        <h1>마음의 편지+</h1>
-        <button>중대</button>
-        <button>대대</button> <br></br>
+        <div className="Ypage">
+        <h2 style={hTwoStyle}>마음의 편지+</h2>
+
+        <div className="Yscale">
+        <button style={buttonOne}>중대</button>
+        <button style={buttonOne}>대대</button> 
+        
+        
+        </div>
+      
         <button>진행중인 글</button>       
         <button>완료된 글</button>
+        <br></br>
 
-        <div className="content">
-            <table>
+
+        <div className="Ycontent">
+            <table className="Ytable">
                 <thead>
-                    <th>    제목     </th>
-                    <th>    날짜     </th>
-                    <th>    진행상태     </th>
+                    <th style={thStyle}>    제목     </th>
+                    <th style={thStyle}>    날짜     </th>
+                    <th style={thStyle}>    진행상태     </th>
                 </thead>
-                <tbody>
-                    <td></td>
+              
+                <tbody >
+                    <tr>
+                    <td style={tdStyle}>비밀 글입니다.</td> 
+                    <td style={tdStyle}>21.09.21</td> 
+                    <td style={tdStyle}>확인완료</td> 
+                    </tr>
+
+                    <tr>
+                    <td style={tdStyle}>비밀 글입니다.</td> 
+                    <td style={tdStyle}>21.09.21</td> 
+                    <td style={tdStyle}>확인완료</td> 
+                    </tr>
+
+
+                    <tr>
+                    <td >비밀 글입니다.</td> 
+                    <td>21.09.21</td> 
+                    <td>확인완료</td> 
+                    </tr>
                 </tbody>
             </table>
         </div>
-
+      
+        <div className="YselectBox">
+                <button style={buttonTwo}>{"<"}</button>  
+             <button style={buttonTwo}>1</button>  
+             <button style={buttonTwo}>2</button>  
+             <button style={buttonTwo}> 3</button>  
+             <button style={buttonTwo}>4</button>  
+             <button style={buttonTwo}>5</button>  
+             <button style={buttonTwo}>{">"}</button>  
+             <button style={buttonTwo}>글 작성</button>
+              
+                </div>
+</div>
         </>
     );
 };
