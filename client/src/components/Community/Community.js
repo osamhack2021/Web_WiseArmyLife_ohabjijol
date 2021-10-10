772c64112975887be62eb526bc52be82e5da6390
hereokay <hereokay88@gmail.com>
2021-10-10 19:45:48 +0900

    커뮤 조금 수정

--- client/src/components/Community/Community.js
+++ client/src/components/Community/Community.js
@@ -3,6 +3,7 @@ import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Forum from './Forum';
 import Post from './Post';
 import Page from './Page';
+import './Community.css'
 
 // match.params 사용
 // 
@@ -12,11 +13,11 @@ const Community = ({match}) => {
 
     return (
         <div>
-            <h2>커뮤니티 + </h2>
+            <h2 className='fourmTitle'>커뮤니티 + </h2>
             <Router>
                 <Route exact path="/Community/:forumId/v/postIndex" component={Post} />
                 <Route exact path="/Community/:forumId/:page?" component={Page} />
-                <Route exact path="/Community" render={() => <Forum />} />
+                <Route exact path="/Community" component={Forum} />
             </Router>
         </div>
     );
