14170ebdde7d925b24f5db79fb1af35b4e5ba5bf
taehyun08 <didls1643@naver.com>
2021-10-11 14:36:14 +0900

    캐싱제거

--- server/passport/index.js
+++ server/passport/index.js
@@ -5,8 +5,6 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const User = require('../models/users')
 
-let cachedUser = {};
-global.cachedUser = cachedUser
 
 passport.use('local-login', new LocalStrategy({
     usernameField: 'militaryNumber',
@@ -15,18 +13,18 @@ passport.use('local-login', new LocalStrategy({
     passReqToCallback: false, // callback함수에 req를 인자로 넘겨줌
 }, async (militaryNumber, password, done) => {
     try {
-        const exUser = await User.findOne({ where: { militaryNumber }});
-        if(exUser){
+        const exUser = await User.findOne({ where: { militaryNumber } });
+        if (exUser) {
             const check = await bcrypt.compare(password, exUser.password);
-            if(check){
+            if (check) {
                 done(null, exUser);
             } else {
                 done(null, false, { message: '비밀번호가 일치하지 않습니다.' });
             }
         } else {
-            done(null, false, { message: '가입되지 않은 회원입니다.'});
+            done(null, false, { message: '가입되지 않은 회원입니다.' });
         }
-    } catch(error) {
+    } catch (error) {
         console.error(error);
         done(error);
     }
@@ -38,17 +36,13 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-    if (Object.keys(cachedUser).length !== 0) {
-        done(null, cachedUser.user);
-    } // 유저 정보 캐싱
-    else {
-        User.findOne({ where: { id } })
-            .then(user => {
-                cachedUser.user = user;
-                done(null, user);
-            })
-            .catch(err => done(err));
-    }
+    User.findOne({ where: { id } })
+        .then(user => {
+            cachedUser.user = user;
+            done(null, user);
+        })
+        .catch(err => done(err));
+
 });
 
 module.exports = passport;
