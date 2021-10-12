f326865a1c786a303ffbda82bf92d69abe952cd3
codrae <88239626+codrae@users.noreply.github.com>
2021-10-12 06:53:35 +0000

    Made NewPost

--- client/src/components/Community/NewPost.js
+++ client/src/components/Community/NewPost.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import "./NewPost.css";
 
 const NewPost = () => {
 
     return (
-        <div>
-         글쓰기
+        <>
+        <h2> 공지사항 +</h2>
+ 
+        <div className="content">
+          <div className="contentTop">
+            <h3>제목 : </h3>
+            <input type="text" name="title" placeholder="제목을 입력하십시오."/>
+       </div>
+ 
+       <div id="mainContent">
+            <input type="text" name="Write" placeholder="내용을 입력하십시오."/>
+                    </div>
+ 
+                    <button>등록</button>
         </div>
+ 
+ <div className="buttonList">
+ <button>{"<"}</button>  
+             <button >1</button>  
+             <button >2</button>  
+             <button > 3</button>  
+             <button >4</button>  
+             <button>5</button>  
+             <button>{">"}</button>  
+ </div>
+ 
+        </>
     );
 };
 
