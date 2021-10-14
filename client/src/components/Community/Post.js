7617ef7a4e3986717bf22fc46686d67457a59cd2
hereokay <hereokay88@gmail.com>
2021-10-14 23:47:11 +0900

    댓글 기능 추가 err 라우터모름

--- client/src/components/Community/Post.js
+++ client/src/components/Community/Post.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { useEffect } from 'react';
 import  axios  from 'axios';
+import { useState } from 'react';
 
 
 
@@ -11,6 +12,7 @@ const Post = (props) => {
     
     const {forumId,postId} = match.params    
 
+    const [comment,setComment] = useState('')
     useEffect(()=>{
         axios.get(`/community/${forumId}/post/v/${postId}`)
         .then(res=>{
@@ -29,13 +31,23 @@ const Post = (props) => {
     const onEdit = ()=>{
         
     }
+    const onCommit = ()=>{
+        const data = {
+            "comment":comment
+        }
+
+        axios.post(`/community/${forumId}/${postId}/comment`,data)
+        .then(res=>{
+            console.log(res.data)
+        })
+    }
     return (
         <div>
             <button onClick={()=>console.log(forumId)}>콘솔</button>
             <button onClick={onRemove}>삭제하기</button>
             <button onClick={onEdit}>수정하기</button>
-            <input placeholder='coment'/>
-            <button>댓글작성</button>
+            <input onChange={(data)=>setComment(data.value)} value={comment} placeholder='coment'/>
+            <button onClick={onCommit}>댓글작성</button>
             <button onClick={()=>history.goBack()}>돌아가기</button>
         </div>
     );
