c6e97815b265b9dee9dfaac9d89409ae68b5adec
SeungBeom Hu <hsb0423@naver.com>
2021-12-12 16:54:35 +0900

    Java_spring으로 서버 개발

---
+++ Web_spring/src/main/java/com/be_spring/osam/WebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.be_spring.osam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpringApplication.class, args);
+    }
+
+}
---
+++ Web_spring/src/test/java/com/be_spring/osam/WebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be_spring.osam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
